21cfe015abe80bf42b3e47c2e06a80e943b01bef
Jake Pine <jpinedev@gmail.com>
2020-01-26 16:40:44 -0500

    initial framework for displaying multiple canvases

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ let sources = [];
 let size = [64, 64, 16, 16];
 let layers = [];
 
+let views = [];
+
 ipc.on('update-tilemap', (event, tm) => {
     sources = tm[0];
     size = tm[1];
@@ -32,20 +34,36 @@ let c = editor.getContext('2d');
 
 editorDiv.scroll(win.scrollWidth/4, win.scrollHeight/4);
 
-function init() {
-    update();
-}
+views = layers.map(_ => {
+    let e = document.createElement('canvas', { style: `margin: -100%; width: ${size[0]}vh; height ${size[1]}vh;` });
+    win.appendChild(e);
+    return { canvas: e, ctx: e.getContext('2d') };
+});
 
-function update() {
+function init() {
+    c.fillStyle = 'rgba(255, 255, 255, .5)';
+    c.fillRect(0, 0, size[0]*size[2], size[1]*size[3]);
     renderTilemap();
 }
 
-function renderTilemap(scale) {
-    c.fillStyle = 'rgba(255, 255, 255, .25)';
-    c.strokeStyle = 'rgba(0, 0, 0, .25)';
-    c.fillRect(0, 0, size[0]*size[2], size[1]*size[3]);
+function renderTilemap() {
+    for(let i = 0; i < layers.length; i++) {
+        renderLayer(i);
+    }
+}
+
+function renderLayer(layerIndex) {
+    const layer = layers[layerIndex].map;
+    const canvas = views[layerIndex].canvas;
+    const ctx = views[layerIndex].ctx;
+    const tileWidth = canvas.width/size[0];
+    const tileHeight = canvas.height/size[1];
     for(let j = 0; j < size[1]; j++) {
-        
+        for(let i = 0; i < size[0]; i++) {
+            const value = layer[j][i];
+            ctx.fillStyle = (value == 0 ? '#000000':'#ffffff');
+            ctx.fillRect(i*tileWidth, j*tileHeight, tileWidth, tileHeight);
+        }
     }
 }
 
